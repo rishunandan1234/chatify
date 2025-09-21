0a148fa2cdd0ed2b9eff29264bd5350382571608
rishunandan1234 <rishunandan89@gmail.com>
2025-09-21 14:38:07 +0530

    change in login

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -84,7 +84,7 @@ export const login = async (req, res) => {
     const {email, password} = req.body;
 
     if(!email || !password){
-        return res.status(400).json({message:"Email and password are required"});
+        return res.status(400).json({message:"Email and password are required."});
     }
 
     try {
